e2ca7b0aaa2e77171f1615c6fe8761c341476447
Jorje Barrera <jorje.barrera@workmate.asia>
2023-03-30 16:53:03 +0800

    Updates

--- app/src/main/java/mmdc/LinkedList.java
+++ app/src/main/java/mmdc/LinkedList.java
@@ -1,16 +1,14 @@
 package mmdc;
 
-import java.util.Iterator;
-
 public class LinkedList {
     public void run() {
-        java.util.LinkedList<String> linkedList = new java.util.LinkedList<String>();
+        var linkedList = new java.util.LinkedList<String>();
         linkedList.add("test1");
         linkedList.add("test2");
         linkedList.add("test3");
         linkedList.add("test4");
 
-        Iterator<String> iterator = linkedList.iterator();
+        var iterator = linkedList.iterator();
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
--- app/src/main/java/mmdc/Stack.java
+++ app/src/main/java/mmdc/Stack.java
@@ -2,7 +2,7 @@ package mmdc;
 
 public class Stack {
     public void run() {
-        java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
+        var stack = new java.util.Stack<Integer>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
